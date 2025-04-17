8b003d78f4a27ab527481f48fb3ddca83824ca9b
wesleyfernando1 <wf243540@gmail.com>
2025-04-16 21:30:28 -0300

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+let quantidadeTotal = 0;
+
 function adicionarAoCarrinho(produto) {
   const lista = document.getElementById("itens-carrinho");
   const item = document.createElement("li");
   item.textContent = produto;
   lista.appendChild(item);
+
+  quantidadeTotal++;
+  document.getElementById("quantidade").textContent = quantidadeTotal;
+}
+
+function filtrarProdutos(categoria) {
+  const produtos = document.querySelectorAll('.produto');
+
+  produtos.forEach(produto => {
+    if (categoria === 'todos' || produto.classList.contains(categoria)) {
+      produto.style.display = 'block';
+    } else {
+      produto.style.display = 'none';
+    }
+  });
 }
